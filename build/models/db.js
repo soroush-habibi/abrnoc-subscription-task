26a441b5132616dda8fd60eef936c0845e9c855b
Soroush Habibi <soroush.habibi.1382@gmail.com>
2023-03-08 12:38:24 +0330

    add register and login method to database module

--- build/models/db.js
+++ build/models/db.js
@@ -1,7 +1,48 @@
 import mongodb from 'mongodb';
+import bcrypt from 'bcrypt';
 export default class db {
     static async connect(func) {
         this.client = await mongodb.MongoClient.connect(process.env.MONGO || "mongodb://127.0.0.1:27017");
         func(this.client);
+    }
+    static async existsUser(username) {
+        const user = await this.client.db("abrnoc").collection("customer").countDocuments({ username: username });
+        return user > 0 ? true : false;
+    }
+    static async registerUser(username, password) {
+        if (username.length < 5) {
+            throw new Error("Username should be bigger than 4 character");
+        }
+        else if (password.length < 5) {
+            throw new Error("Password should be bigger than 4 character");
+        }
+        else if (await this.existsUser(username)) {
+            throw new Error("This username already exists");
+        }
+        const result = await this.client.db("abrnoc").collection("customer").insertOne({
+            username: username,
+            password: bcrypt.hashSync(password, 10),
+            credit: 100,
+            subs: []
+        });
+        return result.acknowledged;
+    }
+    static async loginUser(username, password) {
+        if (username.length < 5) {
+            throw new Error("Username should be bigger than 4 character");
+        }
+        else if (password.length < 5) {
+            throw new Error("Password should be bigger than 4 character");
+        }
+        const user = await this.client.db("abrnoc").collection("customer").findOne({ username: username });
+        if (!user) {
+            throw new Error("Can't find user");
+        }
+        else if (bcrypt.compareSync(password, user.password)) {
+            return user._id;
+        }
+        else {
+            throw new Error("Password is wrong");
+        }
     }
 }
--- source/models/db.ts
+++ source/models/db.ts
@@ -1,4 +1,13 @@
 import mongodb from 'mongodb';
+import bcrypt from 'bcrypt';
+
+interface user {
+    _id: mongodb.ObjectId,
+    username: string,
+    password: string,
+    credit: number,
+    subs: mongodb.ObjectId[]
+}
 
 export default class db {
     private static client: mongodb.MongoClient
@@ -6,5 +15,48 @@ export default class db {
         this.client = await mongodb.MongoClient.connect(process.env.MONGO || "mongodb://127.0.0.1:27017");
 
         func(this.client);
+    }
+
+    static async existsUser(username: string): Promise<boolean> {
+        const user = await this.client.db("abrnoc").collection("customer").countDocuments({ username: username });
+
+        return user > 0 ? true : false;
+    }
+
+    static async registerUser(username: string, password: string): Promise<boolean> {
+        if (username.length < 5) {
+            throw new Error("Username should be bigger than 4 character");
+        } else if (password.length < 5) {
+            throw new Error("Password should be bigger than 4 character");
+        } else if (await this.existsUser(username)) {
+            throw new Error("This username already exists");
+        }
+
+        const result = await this.client.db("abrnoc").collection("customer").insertOne({
+            username: username,
+            password: bcrypt.hashSync(password, 10),
+            credit: 100,
+            subs: []
+        });
+
+        return result.acknowledged;
+    }
+
+    static async loginUser(username: string, password: string): Promise<mongodb.ObjectId> {
+        if (username.length < 5) {
+            throw new Error("Username should be bigger than 4 character");
+        } else if (password.length < 5) {
+            throw new Error("Password should be bigger than 4 character");
+        }
+
+        const user = await this.client.db("abrnoc").collection<user>("customer").findOne({ username: username });
+
+        if (!user) {
+            throw new Error("Can't find user");
+        } else if (bcrypt.compareSync(password, user.password)) {
+            return user._id;
+        } else {
+            throw new Error("Password is wrong");
+        }
     }
 }
