4a710ee549a0abb6834fa3042f41376d36054567
Soroush Habibi <soroush.habibi.1382@gmail.com>
2023-03-09 22:57:44 +0330

    add logout button in frontend and complete functionality of that

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,6 +1,21 @@
 const username = document.querySelector("#username");
 const id = document.querySelector("#id");
 const credit = document.querySelector("#credit");
+const logout = document.querySelector("#logout");
+
+logout.addEventListener('click', async () => {
+    try {
+        const { data } = await axios.get("/api/logout");
+
+        if (data.success) {
+            document.location = "/";
+        } else {
+            alert(data.message)
+        }
+    } catch (e) {
+        alert(e.response.data.message)
+    }
+});
 
 async function loadData() {
     try {
